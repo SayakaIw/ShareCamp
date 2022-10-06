1479b31aa3a80cf640fabc2bf5776ac79371d660
SayakaIw <160105syk@gmail.com>
2022-10-06 09:04:32 +0000

    退会処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :end_user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,20 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def end_user_state # 退会しているかを判断するメソッド
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @end_user = EndUser.find_by(email: params[:end_user][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if!@end_user
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致かつ退会してる
+    if @end_user.valid_password?(params[:end_user][:password])&& @end_user.is_deleted == "withdrawal"
+      ##trueだった場合、退会しているのでサインアップ画面に遷移する
+      flash[:alert] = "このアカウントは退会済みです。"
+      redirect_to new_end_user_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
