f37a1066ef00677e6479ee8ff6453cb37eeee3d2
SayakaIw <160105syk@gmail.com>
2022-10-15 05:04:07 +0000

    top、index、完成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,8 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @sites = Site.order(created_at: :desc).limit(3)
+    # @items = Item.page(params[:page]).per(4).order(created_at: :desc)
   end
 
   def about
